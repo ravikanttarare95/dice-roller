bdb146dcdbec700c0acdd4a62bfe582d66a4be5b
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-05 11:49:41 +0530

    Normal UI created

---
+++ script.js
@@ -0,0 +1,24 @@
+function playGame() {
+  const mainDice = document.getElementById("main-dice");
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+  switch (randomNumber) {
+    case 1:
+      mainDice.src = "./images/dice-1-red.png";
+      break;
+    case 2:
+      mainDice.src = "./images/dice-2-red.png";
+      break;
+    case 3:
+      mainDice.src = "./images/dice-3-red.png";
+      break;
+    case 4:
+      mainDice.src = "./images/dice-4-red.png";
+      break;
+    case 5:
+      mainDice.src = "./images/dice-5-red.png";
+      break;
+    case 6:
+      mainDice.src = "./images/dice-6-red.png";
+      break;
+  }
+}
