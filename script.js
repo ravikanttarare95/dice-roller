9a0f7f633e519a2468786c092c1dd69c7f03f7cb
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-05 14:53:24 +0530

    Modification

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const mainDice = document.getElementById("main-dice");
+const gameScore = document.getElementById("score");
+// function chooseRed() {
+//   mainDice.src = "./images/dice-1-red.png";
+//   gameScore.innerText = "-";
+// }
+// function chooseSilver() {
+//   mainDice.src = "./images/dice-1-silver.png";
+//   gameScore.innerText = "-";
+// }
+
 function playGame() {
   const mainDice = document.getElementById("main-dice");
   const gameScore = document.getElementById("score");
@@ -8,6 +19,7 @@ function playGame() {
   }, 500); // Duration of the animation in milliseconds
 
   const randomNumber = Math.floor(Math.random() * 6) + 1;
+  //   if (mainDice.includes("red.png")) {
   switch (randomNumber) {
     case 1:
       mainDice.src = "./images/dice-1-red.png";
@@ -52,4 +64,51 @@ function playGame() {
       }, 500);
       break;
   }
+  //   } else if (mainDice.includes("silver.png")) {
+  //     const randomNumber = Math.floor(Math.random() * 6) + 1;
+  //     switch (randomNumber) {
+  //       case 1:
+  //         mainDice.src = "./images/dice-1-silver.png";
+  //         setTimeout(() => {
+  //           gameScore.innerText = "1";
+  //         }, 500);
+  //         //   gameScore.innerText = "1";
+  //         break;
+  //       case 2:
+  //         mainDice.src = "./images/dice-2-silver.png";
+  //         //   gameScore.innerText = "2";
+  //         setTimeout(() => {
+  //           gameScore.innerText = "2";
+  //         }, 500);
+  //         break;
+  //       case 3:
+  //         mainDice.src = "./images/dice-3-silver.png";
+  //         //   gameScore.innerText = "3";
+  //         setTimeout(() => {
+  //           gameScore.innerText = "3";
+  //         }, 500);
+  //         break;
+  //       case 4:
+  //         mainDice.src = "./images/dice-4-silver.png";
+  //         //   gameScore.innerText = "4";
+  //         setTimeout(() => {
+  //           gameScore.innerText = "4";
+  //         }, 500);
+  //         break;
+  //       case 5:
+  //         mainDice.src = "./images/dice-5-silver.png";
+  //         //   gameScore.innerText = "5";
+  //         setTimeout(() => {
+  //           gameScore.innerText = "5";
+  //         }, 500);
+  //         break;
+  //       case 6:
+  //         mainDice.src = "./images/dice-6-silver.png";
+  //         //   gameScore.innerText = "6";
+  //         setTimeout(() => {
+  //           gameScore.innerText = "6";
+  //         }, 500);
+  //         break;
+  //     }
+  //   }
 }
